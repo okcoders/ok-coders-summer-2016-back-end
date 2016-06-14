5f4257b9607e47d95fad43262cedf20a8853bb9c
Watson <watson@ou.edu>
2016-06-14 17:25:07 -0500

    june14 updates

--- 01-javascript-intro/examples/values-of-variables.js
+++ 01-javascript-intro/examples/values-of-variables.js
@@ -1,14 +1,14 @@
 // look up how to get parameters from the command line with nodejs. Use that to
 // give values to these variables
 
-var nameOfCurrentFile = // name of current file, i.e values-of-variables.js (get it from the command line)
+var nameOfCurrentFile = "values-of-variables.js"// name of current file, i.e values-of-variables.js (get it from the command line)
 
-var pi = // get this from the command line
+var pi = prompt("enter pi") // get this from the command line
 
-var radius = // get this from the command line (just pick some arbitrary number)
+var radius = prompt("enter radius")// get this from the command line (just pick some arbitrary number)
 
-var area = // fill in the equation utilizing the above variables
+var area = pi * sq(radius)// fill in the equation utilizing the above variables
 console.log(area)
-var cirumference = // fill in the equation utilizing the above variables
+var cirumference = 2 * pi * radius// fill in the equation utilizing the above variables
 console.log(circumference)
 
